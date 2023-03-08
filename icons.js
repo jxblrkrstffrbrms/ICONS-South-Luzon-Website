2a7444d4b87c5824fff20fc20b53b17d2ee5b716
Mary Joyce Llabres <maryjoyce.llabres@g.batstate-u.edu.ph>
2023-03-08 08:39:04 +0800

    login functionality

---
+++ icons.js
@@ -0,0 +1,98 @@
+
+
+async function login() {
+    username = document.getElementById('username').value;
+    password = document.getElementById('password').value;
+
+    if (!username || !password) {
+        alert("Please fill missing input");
+        return
+    }
+
+    // we will change the url of this once we get to deploy our API
+    await fetch('http://127.0.0.1:8080/icons/admins', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"username": username, "password": password})
+    })
+       .then(response => response.json())
+       .then(response => {
+            if (response.message == 'OK') {
+                alert('redirect to admin home page')
+            }
+
+            /*
+            if (response.message == 'OK') {
+                sessionStorage.setItem("token", response.token);
+            }
+            */
+       })
+}
+
+
+async function getActivities() {
+
+    fetch("http://127.0.0.1:8080/icons/activities")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        let activity_list = `<div class="row">`;
+        acts = document.getElementById('activities').innerHTML;
+        let counter = 1;
+        for (const activity of data) { 
+            activity_list += `<div class="card" style="width: 18rem;">
+                            <img class="card-img-top" src="${activity.image_url}" alt="Card image cap">
+                            <div class="card-body">
+                            <h5 class="card-title">${activity.title}</h5>
+                            <p class="card-text">${activity.text}</p>
+                            <a href="#" class="btn btn-primary">More details</a>
+                            </div>
+                        </div>`
+            
+            if (counter % 3 == 0) {
+                activity_list += `</div> <div class="row">`
+            }
+        }
+        document.getElementById('activities').innerHTML = activity_list
+    });
+}
+
+
+async function createActivity() {
+
+    image_url = document.getElementById('image_url').value;
+    title = document.getElementById('title').value;
+    text = document.getElementById('text').value;
+
+
+    await fetch('http://127.0.0.1:8080/icons/activities', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                "image_url": image_url,
+                "title": title,
+                "text": text
+            }
+        )
+    })
+       .then(response => response.json())
+       .then(response => {
+            if ('success' in response) {
+                alert('redirect to admin home page')
+            }
+
+            /*
+            if (response.message == 'OK') {
+                sessionStorage.setItem("token", response.token);
+            }
+            */
+       })
+}
+
